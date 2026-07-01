f8981260348240c1762f687ecfbf5efd10e214c9
wuyifeishu <407248539@qq.com>
2026-07-01 11:24:41 +0800

    Add agentos/feedback/__init__.py

---
+++ agentos/feedback/__init__.py
@@ -0,0 +1,5 @@
+"""Feedback learning: preference collection and learning from signals."""
+
+from .learner import FeedbackCollector, FeedbackRecord, FeedbackType, PreferenceLearner
+
+__all__ = ["FeedbackCollector", "FeedbackRecord", "FeedbackType", "PreferenceLearner"]
