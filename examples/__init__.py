ec2025bb97184a905aaf75ee9ec96143e862866a
wuyifeishu <407248539@qq.com>
2026-07-01 11:30:17 +0800

    Add examples/__init__.py

---
+++ examples/__init__.py
@@ -0,0 +1,6 @@
+"""AgentOS End-to-End Examples."""
+
+from agentos.examples.multi_agent_research import run_swarm_pipeline as multi_agent_research
+from agentos.examples.file_ops_agent import run_file_ops_agent as file_ops_agent
+
+__all__ = ["multi_agent_research", "file_ops_agent"]
