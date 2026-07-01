3f794f9e89461f979b524c48a5bcf4d9948c0969
wuyifeishu <407248539@qq.com>
2026-07-01 11:24:38 +0800

    Add agentos/experiments/__init__.py

---
+++ agentos/experiments/__init__.py
@@ -0,0 +1,12 @@
+"""Experiment runner: prompt variants, trials, evaluation."""
+
+from .runner import ExperimentConfig, ExperimentReport, ExperimentRunner, PromptVariant, TrialResult, Evaluator
+
+__all__ = [
+    "ExperimentConfig",
+    "ExperimentReport",
+    "ExperimentRunner",
+    "PromptVariant",
+    "TrialResult",
+    "Evaluator",
+]
