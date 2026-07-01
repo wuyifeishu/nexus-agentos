1f5004c63c86065aac548516f698e5946337d123
wuyifeishu <407248539@qq.com>
2026-07-01 11:21:56 +0800

    Add agentos/channels/adapters/__init__.py

---
+++ agentos/channels/adapters/__init__.py
@@ -0,0 +1,23 @@
+from agentos.channels.adapters.wechat import WeChatAdapter
+from agentos.channels.adapters.wecom import WeComAdapter
+from agentos.channels.adapters.feishu import FeishuAdapter
+from agentos.channels.adapters.dingtalk import DingTalkAdapter
+from agentos.channels.adapters.qq import QQAdapter
+from agentos.channels.adapters.slack import SlackAdapter
+from agentos.channels.adapters.discord import DiscordAdapter
+from agentos.channels.adapters.telegram import TelegramAdapter
+from agentos.channels.adapters.whatsapp import WhatsAppAdapter
+from agentos.channels.adapters.line import LINEAdapter
+
+__all__ = [
+    "WeChatAdapter",
+    "WeComAdapter",
+    "FeishuAdapter",
+    "DingTalkAdapter",
+    "QQAdapter",
+    "SlackAdapter",
+    "DiscordAdapter",
+    "TelegramAdapter",
+    "WhatsAppAdapter",
+    "LINEAdapter",
+]
