b8c8b12a13561a29c0ae15f881b9cc8436a79d6b
wuyifeishu <407248539@qq.com>
2026-07-01 11:22:41 +0800

    Add agentos/comm/__init__.py

---
+++ agentos/comm/__init__.py
@@ -0,0 +1,15 @@
+"""Communication module - Communication layer"""
+
+from agentos.comm.layer import (
+    CommunicationLayer,
+    Blackboard,
+    EventBus,
+    Mailbox,
+)
+
+__all__ = [
+    "CommunicationLayer",
+    "Blackboard",
+    "EventBus",
+    "Mailbox",
+]
